2953e51a742ab4bed1ea703672d4dcd9274a88f0
quentinformatique <quentin161204@gmail.com>
2025-08-24 23:28:57 +0200

    fix: remove logs

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// Silence all console output in production (no logs in browser console)
+if (process.env.NODE_ENV === 'production') {
+  const noop = () => {};
+  // eslint-disable-next-line no-console
+  console.log = noop;
+  // eslint-disable-next-line no-console
+  console.info = noop;
+  // eslint-disable-next-line no-console
+  console.debug = noop;
+  // eslint-disable-next-line no-console
+  console.warn = noop;
+  // eslint-disable-next-line no-console
+  console.error = noop;
+}
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
